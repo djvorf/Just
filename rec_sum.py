a08313482893f948491f79982337a69d12cdbee5
Old Hokage <djvorf@gmail.com>
2020-05-31 10:31:54 +0600

    add rec alg

---
+++ rec_sum.py
@@ -0,0 +1,22 @@
+def sum(list):
+	if list == []:
+		return 0
+	else:
+		return list[0] + sum(list[1:])
+print(sum([1,24,31]))
+
+
+def count(list):
+	if list == []:
+		return 0
+	else:
+		return 1 + count(list[1:])
+print(count([1,24,31]))
+
+
+def max(list):
+	if len(list) == 2:
+		return list[0] if list [0] > list[1] else list[1]
+	sub_max = max(list[1:])
+	return list[0] if list[0] > sub_max else sub_max
+print(max([1,24,31,32,125,53]))
